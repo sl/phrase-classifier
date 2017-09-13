8b0171c1209241703910e462591b6daed512e86f
Sam Lazarus <tyu.sam@gmail.com>
2017-09-13 19:58:04 -0400

    Added best full gram subset algorithm

--- sentencefinder.py
+++ sentencefinder.py
@@ -33,11 +33,11 @@ def construct_graph_from_grams(grams):
 
         for connection in end_map[indexes[0] - 1]:
             adj_list[connection][0].append((i, score, line_up[i]))
-            adj_list[i][1].append(connection)
+            adj_list[i][1].append((connection, score))
 
     for endConnection in end_map[highest_end]:
         adj_list[endConnection][0].append((len(line_up), 0, None))
-        adj_list[len(adj_list) - 1][1].append(endConnection)
+        adj_list[len(adj_list) - 1][1].append((endConnection, 0))
 
     return adj_list
 
@@ -65,6 +65,51 @@ def topological_sort(graph):
     return list(reversed(ordered))
 
 
+# finds the longest path through the graph
+def longest_path(graph):
+    top_ordering = topological_sort(graph)
+
+    dist = [-float('inf')] * len(graph)
+    dist[0] = 0
+
+    for node in top_ordering:
+        # get the list of adjacent vertices
+        for (connection, weight, val) in graph[node][0]:
+            if dist[connection] < dist[node] + weight:
+                dist[connection] = dist[node] + weight
+
+    # backtrack to determine the best path
+    path = []
+    current = len(graph) - 1  # start at the last node in the graph
+    while current != 0:
+        path.append(current)
+        links_back = graph[current][1]
+        for (back_to, score) in links_back:
+            if dist[back_to] + score == dist[current]:
+                current = back_to
+                break
+    path.append(0)
+    return list(reversed(path))
+
+
+# gets the highest ranking ngram set given a list of ngrams and
+# their placement and scoring information
+def best_full_gram_subset(grams):
+    graph = construct_graph_from_grams(grams)
+    path = longest_path(graph)
+
+    # go through the graph and pull out the ngrams corresponding to
+    # the chosen path
+    maximal_gram_set = []
+
+    # remove the start and end node from the path as they don't have
+    # corresponding ngrams, then get the ngram for each piece of the path
+    for node_index in path[1:-1]:
+        (gram, indexes, rank) = grams[node_index - 1]
+        maximal_gram_set.append(gram)
+    return maximal_gram_set
+
+
 class TestConstructGraphFromNGrams(unittest.TestCase):
 
     def test_sample(self):
@@ -78,27 +123,51 @@ class TestConstructGraphFromNGrams(unittest.TestCase):
 
         expected = [
             ([(1, 2, ('a', [0], 2)), (4, 3, ('ab', [0, 1], 3))], []),
-            ([(2, 2, ('b', [1], 2)), (5, 4, ('bc', [1, 2], 4))], [0]),
-            ([(3, 1, ('c', [2], 1))], [1]),
-            ([(6, 0, None)], [2, 4]),
-            ([(3, 1, ('c', [2], 1))], [0]),
-            ([(6, 0, None)], [1]),
-            ([], [3, 5])
+            ([(2, 2, ('b', [1], 2)), (5, 4, ('bc', [1, 2], 4))], [(0, 2)]),
+            ([(3, 1, ('c', [2], 1))], [(1, 2)]),
+            ([(6, 0, None)], [(2, 1), (4, 1)]),
+            ([(3, 1, ('c', [2], 1))], [(0, 3)]),
+            ([(6, 0, None)], [(1, 4)]),
+            ([], [(3, 0), (5, 0)])
         ]
         self.assertEqual(expected, construct_graph_from_grams(test_grams))
 
     def test_topological_sort(self):
         graph = [
             ([(1, 2, ('a', [0], 2)), (4, 3, ('ab', [0, 1], 3))], []),
-            ([(2, 2, ('b', [1], 2)), (5, 4, ('bc', [1, 2], 4))], [0]),
-            ([(3, 1, ('c', [2], 1))], [1]),
-            ([(6, 0, None)], [2, 4]),
-            ([(3, 1, ('c', [2], 1))], [0]),
-            ([(6, 0, None)], [1]),
-            ([], [3, 5])
+            ([(2, 2, ('b', [1], 2)), (5, 4, ('bc', [1, 2], 4))], [(0, 2)]),
+            ([(3, 1, ('c', [2], 1))], [(1, 2)]),
+            ([(6, 0, None)], [(2, 1), (4, 1)]),
+            ([(3, 1, ('c', [2], 1))], [(0, 3)]),
+            ([(6, 0, None)], [(1, 4)]),
+            ([], [(3, 0), (5, 0)])
         ]
         expected = [0, 1, 2, 5, 4, 3, 6]
         self.assertEqual(expected, topological_sort(graph))
+
+    def test_longest_path(self):
+        graph = [
+            ([(1, 2, ('a', [0], 2)), (4, 3, ('ab', [0, 1], 3))], []),
+            ([(2, 2, ('b', [1], 2)), (5, 4, ('bc', [1, 2], 4))], [(0, 2)]),
+            ([(3, 1, ('c', [2], 1))], [(1, 2)]),
+            ([(6, 0, None)], [(2, 1), (4, 1)]),
+            ([(3, 1, ('c', [2], 1))], [(0, 3)]),
+            ([(6, 0, None)], [(1, 4)]),
+            ([], [(3, 0), (5, 0)])
+        ]
+        expected_path = [0, 1, 5, 6]
+        self.assertEquals(longest_path(graph), expected_path)
+
+    def test_best_full_gram_subset(self):
+        test_grams = [
+            ('a', [0], 2),
+            ('b', [1], 2),
+            ('c', [2], 1),
+            ('ab', [0, 1], 3),
+            ('bc', [1, 2], 4)
+        ]
+        expected_grams = ['a', 'bc']
+        self.assertEquals(expected_grams, best_full_gram_subset(test_grams))
 
 
 if __name__ == '__main__':
