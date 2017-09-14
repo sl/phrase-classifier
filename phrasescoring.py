271d9c8c263e625fdc2fe47dfcbcec2f9f5415c2
Sam Lazarus <tyu.sam@gmail.com>
2017-09-13 20:15:42 -0400

    Added additional utilities to phrasescoring.py for scoring grams.

--- phrasescoring.py
+++ phrasescoring.py
@@ -1,8 +1,21 @@
 import random
 
 
+# scores all the phrases in the set of two-tuple grams, and attaches
+# the scores as the last element of a three-tuple
+def score_grams(grams):
+    return [attach_score(gram) for gram in grams]
+
+
+# attaches a score to a two-tuple gram as the last element of a three-tuple
+def attach_score(gram):
+    (phrase, indexes) = gram
+    score = score_phrase(phrase)
+    return (phrase, indexes, score)
+
+
 # score an array of words on how relavant it is
 def score_phrase(phrase):
-    return random.uniform(0, 1)  # todo -- actually score phrases
+    return random.uniform(0, 1/3) * len(phrase)
 
 # no unit tests necessary until this actually does something
